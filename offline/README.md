7ba6c64a4a3c4def074327d2cac13898fbfc6819
Pfreym <spcompsys@gmail.com>
2023-07-21 21:10:46 +0800

    Base of Tauri/Svelte with Tailwind and flowbite

---
+++ offline/README.md
@@ -0,0 +1,9 @@
+# To Run 
+
+Please follow steps for your OS https://tauri.app/v1/guides/getting-started/prerequisites
+
+# Dev Server
+
+```
+npm run tauri Dev
+```
---
+++ offline/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { Alert } from 'flowbite-svelte';
+</script>
+
+<div class="p-8">
+	<Alert>
+		<span class="font-medium">Welcome</span> Start building the Notes app from here!
+	</Alert>
+</div>
