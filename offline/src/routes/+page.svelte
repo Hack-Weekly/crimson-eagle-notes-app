6d16f4d83703041baa2bc02b8bddd14b97f69e7d
Caio Mauro <caiomauro18@gmail.com>
2023-07-22 00:21:04 -0400

    UI Start + Modules
    -Added Icons
    -Started creating UI

--- offline/src/routes/+page.svelte
+++ offline/src/routes/+page.svelte
@@ -1,9 +1,34 @@
-<script>
+<script lang='ts'>
 	import { Alert } from 'flowbite-svelte';
+	import {dndzone} from 'svelte-dnd-action';
+
+	function handleAddNote(){
+		console.log("Drop down color circles which when clicked will add buttons");
+	}
+
 </script>
 
-<div class="p-8">
-	<Alert>
-		<span class="font-medium">Welcome</span> Start building the Notes app from here!
-	</Alert>
-</div>
+<main class="h-full w-full flex flex-col content-center justify-center py-10">
+	<div class="flex justify-between w-full pt-8">
+		<div id="add" class="mx-10">
+			<button on:click={handleAddNote}>
+			<svg class="w-10 h-10 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+				<path d="M9.546.5a9.5 9.5 0 1 0 9.5 9.5 9.51 9.51 0 0 0-9.5-9.5ZM13.788 11h-3.242v3.242a1 1 0 1 1-2 0V11H5.304a1 1 0 0 1 0-2h3.242V5.758a1 1 0 0 1 2 0V9h3.242a1 1 0 1 1 0 2Z"/>
+			</svg>
+			<button>
+		</div>
+		<div class="mx-10">
+			<h1 class=" font-serif text-5xl">Nota</h1>
+		</div> 
+		<div id="favorite"class="mx-10">
+			<svg class="w-10 h-10 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+				<path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+			</svg>
+		</div>
+		<div id="user" class="mx-10">
+			<svg class="w-10 h-10 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+				<path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+			</svg>
+		</div>
+	</div>
+</main>
