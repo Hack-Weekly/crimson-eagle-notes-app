56ba2111382b2ec781e30ef45ff61fcabfd9c88c
Ellahi <ellahinator02@gmail.com>
2023-07-26 17:52:46 -0400

    replace Note with NoteType, minor changes to add (using addNote from store now), restructure stores

---
+++ offline/src/lib/stores/note.ts
@@ -0,0 +1,58 @@
+import { writable } from 'svelte/store';
+import { invoke } from '@tauri-apps/api/tauri';
+import type { NoteType, PaginatedNotes } from "../../types/note.type";
+
+export const isLoading = writable(false);
+export const fetchError = writable<string | null>(null);
+
+const loadNotes = async (): Promise<NoteType[]> => {
+    fetchError.set(null);
+    const storedNotes = localStorage.getItem('notes');
+
+    if (!storedNotes) {
+        isLoading.set(true);
+        const fetchNotes = await invoke<PaginatedNotes>('fetch_notes')
+            .then(res => res.records)
+            .catch(err => {
+                console.log(err)
+                fetchError.set(err)
+            })
+            .finally(() => isLoading.set(false));
+        return fetchNotes ? fetchNotes : [];
+    }
+    return JSON.parse(storedNotes);
+};
+
+const saveNotes = (notes: NoteType[]) => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+};
+
+const noteStore = writable<NoteType[]>(await loadNotes());
+
+
+const createNotesStore = () => {
+    const { subscribe, update, set } = noteStore;
+  
+    return {
+      subscribe,
+      setNotes: (notes: NoteType[]) => set(notes),
+      addNote: (note: NoteType) => update(notes => {
+        const newNotes = [...notes, note];
+        saveNotes(newNotes);
+        return newNotes;
+      }),
+      deleteNote: (id: number) => update(notes => {
+        const newNotes = notes.filter(note => note.id !== id);
+        saveNotes(newNotes);
+        return newNotes;
+      }),
+      updateNote: (id: number, updatedFields: Partial<NoteType>) => update(notes => {
+        const newNotes = notes.map(note => note.id === id ? { ...note, ...updatedFields } : note);
+        saveNotes(newNotes);
+        return newNotes;
+      }),
+    };
+};
+  
+  
+export const notes = createNotesStore();
--- offline/src/routes/add/+page.svelte
+++ offline/src/routes/add/+page.svelte
@@ -1,31 +1,31 @@
 <script lang='ts'>
-    import { Label, Input } from 'flowbite-svelte';
+    import { Label } from 'flowbite-svelte';
     import { Button } from 'flowbite-svelte';
+    import { notes } from '$lib/stores/note';
+	import type { NoteType } from '../../types/note.type';
 
-
+    let newNoteTitle = '';
+    let newNoteContent = '';
 
     const addNewNote = () => {
         console.log("Adding new note");
 
-        console.log(localStorage.getItem('noteColor'));
+        let currentNotes = JSON.parse(localStorage.getItem('notes') || '[]');
+        console.log(currentNotes.length);
 
-        const newNoteTitle = document.getElementById('title').value;
-        const newNoteContent = document.getElementById('content').value;
-
-        let currentNotes = localStorage.getItem('notes');
-        currentNotes = currentNotes ? JSON.parse(currentNotes) : [];
-
-        const newNote = {
+        const newNote: NoteType = {
             id: currentNotes.length + 1,
-            color: localStorage.getItem('noteColor'), 
+            color: localStorage.getItem('noteColor')!, 
             title: newNoteTitle,
             excerpt: newNoteContent.substr(0, 50), 
+            content: newNoteContent,
             starred: false, 
+            created_on: new Date(),
+            updated_on: new Date(),
         };
+        console.log(newNote);
 
-        currentNotes.push(newNote);
-
-        localStorage.setItem('notes', JSON.stringify(currentNotes));
+        notes.addNote(newNote);
         
         window.location.href = "/";
     }
@@ -37,9 +37,9 @@
 
 <div class="flex flex-col justify-start items-center gap-4">
     <Label for='large-input' class='block mb-2'>Title</Label>
-    <textarea id='title' class="flex gap-2 w-96 h-10 items-center rounded-lg text-sm focus:outline-none neumorph border-black" placeholder="Title..."/>
+    <textarea bind:value={newNoteTitle} id='title' class="flex gap-2 w-96 h-10 items-center rounded-lg text-sm focus:outline-none neumorph border-black" placeholder="Title..."/>
     <Label for='large-input' class='block mb-2'>Content</Label>
-    <textarea id='content' class="flex gap-2 w-96 h-96 rounded-lg text-sm focus:outline-none neumorph border-black break-words" placeholder="Content"/>
+    <textarea bind:value={newNoteContent} id='content' class="flex gap-2 w-96 h-96 rounded-lg text-sm focus:outline-none neumorph border-black break-words" placeholder="Content"/>
     <div class="flex items-center bottom-0 p-4 gap-8">
         <Button on:click={addNewNote} class="btn"> Save </Button>
         <Button class="btn"><a href="/">Cancel</a></Button>
